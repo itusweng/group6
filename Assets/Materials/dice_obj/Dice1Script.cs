6b07d07d07e8c8a4eef077eb324c1fdf033c4d97
yusufalptigin <yusufalptigin@hotmail.com>
2021-12-20 22:43:18 +0300

    Dice Roll Mechanic
    Dice roll mechanic implemented.

---
+++ Assets/Materials/dice_obj/Dice1Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice1Script : MonoBehaviour{
+    static Rigidbody rb;
+    public static Vector3 dice1Velocity;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        dice1Velocity = rb.velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            float dirX = Random.Range(0, 500);
+            float dirY = Random.Range(0, 500);
+            float dirZ = Random.Range(0, 500);
+            transform.position = new Vector3(0, 7, 2);
+            transform.rotation = Quaternion.identity;
+            rb.AddForce(transform.up * 500);
+            rb.AddTorque(dirX, dirY, dirZ);
+        }
+    }
+}
