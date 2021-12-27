c9d141f0d5516d968878f0e4f2851f51d33bb639
Berktug Ozkan <berktug.ozkan@obss.com.tr>
2021-12-27 21:52:08 +0300

    turn based system implemented

---
+++ Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnHandler : MonoBehaviour
+{
+  public int playerTurn = 0;
+  public GameObject[] players;
+
+  public void RollDice()
+  {
+    MoveScript moveScript = players[playerTurn].GetComponent<MoveScript>();
+    moveScript.Move(MoveScript.Direction.FORWARD);
+    AfterTurn();
+  }
+
+  public void TimeEnded()
+  {
+    Debug.Log("Time has ended random action selecting!!!");
+    AfterTurn();
+  }
+
+  void AfterTurn()
+  {
+    playerTurn = (playerTurn + 1) % players.Length;
+    TurnTimer turnTimer = GetComponent<TurnTimer>();
+    turnTimer.ResetTimer();
+  }
+
+}
---
+++ Assets/Scripts/TurnTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnTimer : MonoBehaviour
+{
+  public Text remainingTimeText;
+
+  public float initialTimeLeft = 60f;
+  float timeLeft;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    timeLeft = initialTimeLeft;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (timeLeft <= 0)
+    {
+      TurnHandler turnHandler = GetComponent<TurnHandler>();
+      turnHandler.TimeEnded();
+    }
+    else
+    {
+      timeLeft -= Time.deltaTime;
+      remainingTimeText.text = "Time remaining: " + timeLeft.ToString("00");
+    }
+  }
+
+  public void ResetTimer()
+  {
+    timeLeft = initialTimeLeft;
+  }
+}
